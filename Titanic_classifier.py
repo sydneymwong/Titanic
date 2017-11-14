f23e70e33d1ff1579d437028801ba4e6684328ea
Michael Knopf <sydneymwong@gmail.com>
2017-11-13 20:16:53 -0800

    added random forest

--- Titanic_classifier.py
+++ Titanic_classifier.py
@@ -2,38 +2,54 @@ import scipy
 import numpy
 import pandas
 from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
 
+# read in training data
 train = pandas.read_csv('train.csv')
 list(train)
+
+# rudimentary cleaning
 train = train.fillna(0)
 
-train['Sex'] = train['Sex'].replace("male", "0")
-train['Sex'] = train['Sex'].replace("female", "1")
-train['Sex'] = train['Sex'].apply(int)
+train['Sex'] = train['Sex'].apply(lambda value: 1 if value == "female" else "0")
+
+# for now, limit training set to numeric variables
 
 train_features = train[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare']]
 
-#train_features = train.drop(['Survived'], axis=1)
-#train_features_dummies = pandas.get_dummies(train_features)
-#survived = train['Survived']
+# create a classification tree using the training data
+
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(train_features, train['Survived'])
 #clf = clf.fit(train_features_dummies, train['Survived'])
 train_predictions = clf.predict(train_features)
 numpy.sum(train_predictions == train['Survived'])
 
-# Read in the test data
+# read in the test data
 test = pandas.read_csv('test.csv')
+
+# repeat same cleaning/subsetting on the test data
 test = test.fillna(0)
 
-test['Sex'] = test['Sex'].replace("male", "0")
-test['Sex'] = test['Sex'].replace("female", "1")
-test['Sex'] = test['Sex'].apply(int)
+test['Sex'] = test['Sex'].apply(lambda value: 1 if value == "female" else "0")
 
 test_features = test[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare']]
 
-#test_features_dummies = pandas.get_dummies(test)
 test_predictions = clf.predict(test_features)
+predictions = pandas.DataFrame(test['PassengerId'])
+predictions = predictions.assign(Survived = test_predictions)
 
+# export pandas dataframe as csv
+predictions.to_csv('submission1.csv', index = False)
 
+# random forest
+clfrf = RandomForestClassifier(n_jobs=1)
+clfrf.fit(train_features, train['Survived'])
+train_predictions_rf = clfrf.predict(train_features)
+numpy.sum(train_predictions_rf == train['Survived'])
+test_predictions_rf = clfrf.predict(test_features)
+predictions_rf = pandas.DataFrame(test['PassengerId'])
+predictions_rf = predictions_rf.assign(Survived = test_predictions_rf)
 
+#export pandas dataframe as csv
+predictions_rf.to_csv('submission2.csv', index = False)
